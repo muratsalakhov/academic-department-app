344a88ff69349b6aa68f60d8bc2d1dbc04bb9dff
muratsalakhov <muratsalakhov@gmail.com>
2020-08-11 21:49:17 +0300

    Апдейт учебного плана

--- demo-app/src/Components/IncomingMessage.js
+++ demo-app/src/Components/IncomingMessage.js
@@ -85,8 +85,81 @@ class IncomingMessage extends React.Component {
                         isLoaded: true,
                         error
                     });
-                }
-            )
+                })
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.groupId !== prevProps.groupId) {
+            fetch(`http://localhost:8080/study_group_plan/${this.props.groupId}`)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        console.log(this.props.groupId)
+                        this.setState({
+                            isLoaded: true,
+                            groupInfo: result
+                        });
+                        fetch(`http://localhost:8080/study_group/${this.state.groupInfo.study_group_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        group: result.name
+                                    });
+                                })
+                        fetch(`http://localhost:8080/subject/${this.state.groupInfo.first_subject_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        firstSubj: result.name
+                                    });
+                                })
+                        fetch(`http://localhost:8080/subject/${this.state.groupInfo.second_subject_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        secondSubj: result.name
+                                    });
+                                })
+                        fetch(`http://localhost:8080/subject/${this.state.groupInfo.third_subject_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        thirdSubj: result.name
+                                    });
+                                })
+                        fetch(`http://localhost:8080/subject/${this.state.groupInfo.fourth_subject_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        fourthSubj: result.name
+                                    });
+                                })
+                        fetch(`http://localhost:8080/subject/${this.state.groupInfo.fifth_subject_id}`)
+                            .then(res => res.json())
+                            .then(
+                                (result) => {
+                                    this.setState({
+                                        fifthSubj: result.name
+                                    });
+                                })
+
+                    },
+                    // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+                    // чтобы не перехватывать исключения из ошибок в самих компонентах.
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                )
+        }
+
     }
 
     render() {
